74a609e78b934604b191b807a8744774a1dff93e
Lauri Lubi <lauri.lubi@lime.tech>
2018-10-05 09:01:31 +0200

    force english on loginpage

--- customization/tretton37/login-login-asp.js
+++ customization/tretton37/login-login-asp.js
@@ -1,8 +1,12 @@
-/* best javascript */
-
 $(function () {
     $("#layer2 table table").each(function () {
-        //        var loginTable = $(this);
+        // var loginTable = $(this);
+        $("select[name=selectLanguage]", this).each(function () {
+            var t = $(this);
+            if (t.val() == "ENG") return;
+            t.val("ENG").change();
+        });
+
         $("tr:has(input[name=server])", this).each(function () {
             $("input[name=server]", this).val("px");
             $(this).hide();
