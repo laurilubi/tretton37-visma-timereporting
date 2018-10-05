ab82312fd6692a5ac7e2963bdedd2c42cc9a9518
Lauri Lubi <lauri.lubi@lime.tech>
2018-10-05 17:27:48 +0200

    login page language

--- customization/tretton37/login-login-asp.js
+++ customization/tretton37/login-login-asp.js
@@ -1,20 +1,25 @@
 $(function () {
     $("#layer2 table table").each(function () {
-        // var loginTable = $(this);
-        $("select[name=selectLanguage]", this).each(function () {
-            var t = $(this);
-            if (t.val() == "ENG") return;
-            t.val("ENG").change();
-        });
-
         $("tr:has(input[name=server])", this).each(function () {
-            $("input[name=server]", this).val("px");
+            $("input", this).val("px");
             $(this).hide();
         });
 
         $("tr:has(input[name=database])", this).each(function () {
-            $("input[name=database]", this).val("tretton37");
+            $("input", this).val("tretton37");
             $(this).hide();
+        });
+
+        $("tr:has(select[name=selectLanguage])", this).each(function () {
+            var t = $(this);
+            t.hide();
+
+            $("select",this).each(function(){
+                var t = $(this);
+                if (t.val() == "ENG") return;
+
+                t.val("ENG").change();
+            });
         });
     })
 });
