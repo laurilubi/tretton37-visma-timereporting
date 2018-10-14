26c94fac55cca31a1d6abcfb2893866c4a8ce032
Lauri Lubi <lauri.lubi@lime.tech>
2018-10-14 16:23:31 +0200

    fix spelling

--- customization/tretton37/timebase-time-report.js
+++ customization/tretton37/timebase-time-report.js
@@ -3,5 +3,9 @@ $(function () {
         $("#timeTable", this).each(function () {
             $("#day_1_1", this).focus();
         });
+
+        $("a.normalbutton:contains('Favourits')", this).each(function () {
+            $(this).text("Favourites");
+        });
     });
 });
