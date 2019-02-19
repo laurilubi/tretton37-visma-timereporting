7b971be012a50b0f51dab501f1e0ebe757b43453
Nikki Sollid <nikki.sollid@gmail.com>
2019-02-19 19:49:24 +0100

    - wip

--- customization/nikki/reportbase-top-mostright.js
+++ customization/nikki/reportbase-top-mostright.js
@@ -1,10 +1,7 @@
 setTimeout(x=>{
     $( document ).ready(function() {
-
-    $('img').attr("src","https://png.pngtree.com/svg/20170825/logout_1185957.png");
-    $('img').attr("width","50");
-
-
-});
+        $('img:nth-child(0)').attr("src","https://png.pngtree.com/svg/20170825/logout_1185957.png");
+        $('img').attr("width","30");
+    });
 },1);
 
