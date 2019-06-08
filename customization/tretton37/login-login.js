8478a873b9520683852a7c5229c1d11fa55327b2
Lauri Lubi <lauri.lubi@tretton37.com>
2019-06-08 18:43:18 +0200

    remove unused code on login page

--- customization/tretton37/login-login.js
+++ customization/tretton37/login-login.js
@@ -1,5 +1,5 @@
 $(function () {
-    
+
     $('#layer2').prepend("<div class='login'></div>");
     $('table').hide();
     $('#username').remove();
@@ -10,31 +10,4 @@ $(function () {
     $('.login').append("<input type='text' id='username' name='username' placeholder='Ninja id'>");
     $('.login').append("<input type='password' id='password' name='password' placeholder='Secret password'>");
     $('.login').append("<div class='loginBtn' onclick='javascript:SubmitForm()'>Login</div>");
-
-
-
-    $("#layer2 table table").each(function () {
-
-        $("tr:has(input[name=server])", this).each(function () {
-            $("input", this).val("px");
-            $(this).hide();
-        });
-
-        $("tr:has(input[name=database])", this).each(function () {
-            $("input", this).val("tretton37");
-            $(this).hide();
-        });
-
-        $("tr:has(select[name=selectLanguage])", this).each(function () {
-            var t = $(this);
-            t.hide();
-
-            $("select",this).each(function(){
-                var t = $(this);
-                if (t.val() == "ENG") return;
-
-                t.val("ENG").change();
-            });
-        });
-    })
 });
