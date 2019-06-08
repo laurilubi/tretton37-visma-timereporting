9c7f3c4db35c40e54e1cb285ee95eb80b1f474ff
Lauri Lubi <lauri.lubi@tretton37.com>
2019-06-08 22:02:45 +0200

    rewrite loader as class

--- customization/loader.js
+++ customization/loader.js
@@ -1,106 +1,111 @@
 'use strict';
 
-main();
+var Extension = function () {
+  var self = this;
+  var baseUrl;
+  var head;
 
-function main() {
-  if (handleRedirects() == false) return;
-  pageInjection();
-}
+  this.execute = function () {
+    if (handleRedirects() == false) return;
 
-function handleRedirects() {
-  if (document.location.href.indexOf("http://pxcontrol1337.afdrift.se/") >= 0) {
-    document.location = "https://pxcontrol1337.afdrift.se/";
-    return false;
-  } else if (document.location.href.indexOf("://px3.afdrift.se/") >= 0) {
-    document.location = "https://pxcontrol1337.afdrift.se/";
-    return false;
+    baseUrl = getExtensionBaseUrl();
+    head = getHead();
+    conditionalInjection();
   }
-  return true;
-}
 
-function pageInjection() {
-  if (getHead() == null) {
-    console.log('head=null - skipping');
+  var handleRedirects = function () {
+    if (window != top) return;
 
-  } else {
-    let hasAlreadyJquery = !!window.$;
-    console.log('hasAlreadyJquery=' + hasAlreadyJquery);
+    if (document.location.href.indexOf("http://pxcontrol1337.afdrift.se/") >= 0) {
+      document.location = "https://pxcontrol1337.afdrift.se/";
+      return false;
+    } else if (document.location.href.indexOf("://px3.afdrift.se/") >= 0) {
+      document.location = "https://pxcontrol1337.afdrift.se/";
+      return false;
+    }
+    return true;
+  }
 
-    if (hasAlreadyJquery === false) {
-      addScript("customization/jquery-3.4.1.min.js", getExtensionBaseUrl(), inject);
-      // addScript("https://code.jquery.com/jquery-3.3.1.min.js");
-      // jQuery.noConflict();
+  var conditionalInjection = function () {
+    if (head == null) {
+      console.log('head=null - skipping');
+
     } else {
-      inject();
+      let hasAlreadyJquery = !!window.$;
+      console.log('hasAlreadyJquery=' + hasAlreadyJquery);
+
+      if (hasAlreadyJquery === false) {
+        addScript("customization/jquery-3.4.1.min.js", inject);
+      } else {
+        inject();
+      }
     }
   }
+
+  var inject = function () {
+    var customization = getCustomization();
+    addCss("customization/" + customization + "/_common.css");
+    addScript("customization/" + customization + "/_common.js");
+
+    var pageId = getPageId(location.pathname);
+    addCss("customization/" + customization + "/" + pageId + ".css");
+    addScript("customization/" + customization + "/" + pageId + ".js");
+  }
+
+  var getCustomization = function () {
+    return "tretton37"; // TODO
+  }
+
+  var getPageId = function (url) {
+    var splits = url.split("?");
+    var normalizedUrl = splits[0];
+    normalizedUrl = normalizedUrl.replace(/[^a-z\d]/gi, "-");
+    if (normalizedUrl.startsWith("-")) normalizedUrl = normalizedUrl.substr(1);
+    if (normalizedUrl.endsWith("-asp")) normalizedUrl = normalizedUrl.substr(0, normalizedUrl.length - 4);
+    return normalizedUrl;
+  }
+
+  var getExtensionBaseUrl = function () {
+    var html = document.getElementsByTagName("html")[0];
+    return html.getAttribute("extensionBaseUrl");
+  }
+
+  var addScript = function (url, callback) {
+    if (head == null) return false;
+
+    var elm = document.createElement("script");
+    elm.src = url.startsWith("http://") || url.startsWith("https://")
+      ? url
+      : baseUrl + url;
+    elm.type = "text/javascript";
+    if (callback) elm.onload = callback;
+    head.appendChild(elm);
+
+    return true;
+  }
+
+  var addCss = function (url, callback) {
+    if (head == null) return false;
+
+    var elm = document.createElement("link");
+    elm.href = baseUrl + url;
+    elm.rel = "stylesheet";
+    elm.type = "text/css";
+    if (callback) elm.onload = callback;
+    head.appendChild(elm);
+
+    return true;
+  }
+
+  var getHead = function () {
+    var heads = document.getElementsByTagName("head");
+    if (heads.length === 0) return null;
+    return heads[0];
+  }
 }
 
-function inject() {
-  let baseUrl = getExtensionBaseUrl();
 
-  var customization = getCustomization();
-  addCss("customization/" + customization + "/_common.css", baseUrl);
-  addScript("customization/" + customization + "/_common.js", baseUrl);
-
-  var pageId = getPageId(location.pathname);
-  addCss("customization/" + customization + "/" + pageId + ".css", baseUrl);
-  addScript("customization/" + customization + "/" + pageId + ".js", baseUrl);
-}
-
-function getCustomization() {
-  return "tretton37"; // TODO
-}
-
-function getPageId(url) {
-  var splits = url.split("?");
-  var normalizedUrl = splits[0];
-  normalizedUrl = normalizedUrl.replace(/[^a-z\d]/gi, "-");
-  if (normalizedUrl.startsWith("-")) normalizedUrl = normalizedUrl.substr(1);
-  if (normalizedUrl.endsWith("-asp")) normalizedUrl = normalizedUrl.substr(0, normalizedUrl.length - 4);
-  return normalizedUrl;
-}
-
-function getExtensionBaseUrl() {
-  var html = document.getElementsByTagName('html')[0];
-  return html.getAttribute("extensionBaseUrl");
-}
-
-function addScript(url, baseUrl, callback) {
-  var head = getHead();
-  if (head == null) return false;
-
-  var elm = document.createElement('script');
-  elm.src = url.startsWith("http://") || url.startsWith("https://")
-    ? url
-    : baseUrl + url;
-  elm.type = "text/javascript";
-  if (callback) elm.onload = callback;
-  head.appendChild(elm);
-
-  return true;
-}
-
-function addCss(url, baseUrl, callback) {
-  var head = getHead();
-  if (head == null) return false;
-
-  var elm = document.createElement('link');
-  elm.href = baseUrl + url;
-  elm.rel = "stylesheet";
-  elm.type = "text/css";
-  if (callback) elm.onload = callback;
-  head.appendChild(elm);
-
-  return true;
-}
-
-function getHead() {
-  var heads = document.getElementsByTagName('head');
-  if (heads.length === 0) return null;
-  return heads[0];
-}
-
+new Extension().execute();
 
 
 // Returns the full url, based on the active customization
