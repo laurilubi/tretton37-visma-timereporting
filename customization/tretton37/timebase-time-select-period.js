6dec1c32baab05910ab8c8364df8d63537f0e204
Lauri Lubi <lauri@lubi.se>
2020-03-28 07:54:27 +0100

    formatting mainly

--- customization/tretton37/timebase-time-select-period.js
+++ customization/tretton37/timebase-time-select-period.js
@@ -1,18 +1,19 @@
-(function($) {
-  $(() => {
-    function idifyTheRadios() {
-      const $radios = $('td input[type="radio"]');
-      $radios.each(function() {
-        const id = $(this).attr("value");
-        $(this).attr("id", id);
-        const $label = $(this)
-          .parent()
-          .next();
-        const labelText = $label.html();
-        const label = $(`<label for="${id}">${labelText}</label>`);
-        $label.html(label);
-      });
-    }
-    idifyTheRadios();
-  });
+(function ($) {
+    $(() => {
+        function idifyRadios() {
+            const $radios = $('td input[type=radio]');
+            $radios.each(function () {
+                const id = $(this).attr('value');
+                $(this).attr('id', id);
+                const $label = $(this)
+                    .parent()
+                    .next();
+                const labelText = $label.html();
+                const label = $(`<label for="${id}">${labelText}</label>`);
+                $label.html(label);
+            });
+        }
+
+        idifyRadios();
+    });
 })(jQuery);
