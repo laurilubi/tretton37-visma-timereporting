5b52bcde714fa492f9f4d059e0edffeeb0d55d68
Lauri Lubi <lauri@lubi.se>
2020-05-02 11:15:46 +0200

    options for customization

--- options.js
+++ options.js
@@ -1,22 +1,69 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+const keyCustomization = 'config.customization';
 
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function () {
-      chrome.storage.sync.set({ color: item }, function () {
-        console.log('color is ' + item);
-      })
+function initCustomization(selectJq) {
+  getCustomizations(function (customizations) {
+    debugger;
+    for (let customization of customizations) {
+      let option = $('<option/>').val(customization).text(customization);
+      option.appendTo(selectJq);
+    }
+
+    chrome.storage.local.get([keyCustomization], function (result) {
+      const customization = result[keyCustomization];
+      console.log(`GET ${keyCustomization}=${customization}`);
+
+      selectJq.val(customization);
+      if (selectJq.val() === customization) return;
+
+      // non-existant value in config
+      selectJq.val('tretton37');
+      persistCustomization(selectJq);
     });
-    page.appendChild(button);
-  }
+
+    selectJq.change(function () {
+      persistCustomization(selectJq);
+    });
+  });
 }
-constructOptions(kButtonColors);
+
+function getCustomizations(callback) {
+  debugger;
+  chrome.runtime.getPackageDirectoryEntry(function (directoryEntry) {
+    debugger;
+    directoryEntry.getDirectory('customization', {}, function (subDirectoryEntry) {
+      debugger;
+      var directoryReader = subDirectoryEntry.createReader();
+      var folderNames = [];
+      (function readNext() {
+        debugger;
+        directoryReader.readEntries(function (entries) {
+          debugger;
+          if (entries.length) {
+            for (var i = 0; i < entries.length; ++i) {
+              if (entries[i].isDirectory==false) continue;
+              folderNames.push(entries[i].name);
+            }
+            readNext();
+          } else {
+            callback(folderNames);
+          }
+        });
+      })();
+    });
+  });
+}
+
+function persistCustomization(selectJq) {
+  const data = {};
+  data[keyCustomization] = selectJq.val();
+  chrome.storage.local.set(data, function () {
+    console.log(`SET ${keyCustomization}=${data[keyCustomization]}`);
+  });
+}
+
+$(function () {
+  const customizationSelectJq = $('select[name=customization]');
+  initCustomization(customizationSelectJq);
+});
