2d012a993c3b74b3d6e29046b6727150b9786e71
Lauri Lubi <lauri@lubi.se>
2020-05-02 16:33:04 +0200

    options page

--- options.js
+++ options.js
@@ -1,69 +1,61 @@
 'use strict';
 
-const keyCustomization = 'config.customization';
+let config = null; // async loaded
 
 function initCustomization(selectJq) {
-  getCustomizations(function (customizations) {
-    debugger;
-    for (let customization of customizations) {
-      let option = $('<option/>').val(customization).text(customization);
-      option.appendTo(selectJq);
-    }
+  getCustomizations()
+    .then(customizations => {
+      for (const customization of customizations) {
+        const option = $('<option/>').val(customization).text(customization);
+        option.appendTo(selectJq);
+      }
 
-    chrome.storage.local.get([keyCustomization], function (result) {
-      const customization = result[keyCustomization];
-      console.log(`GET ${keyCustomization}=${customization}`);
-
+      const customization = config.customization;
+      console.log(`customization=${customization}`);
       selectJq.val(customization);
-      if (selectJq.val() === customization) return;
+      if (selectJq.val() !== customization) {
+        // non-existant value in config
+        customization = config.customization = defaultCustomization;
+        selectJq.val(customization);
+        saveConfig(config);
+      }
 
-      // non-existant value in config
-      selectJq.val('tretton37');
-      persistCustomization(selectJq);
+      selectJq.change(() => {
+        config.customization = selectJq.val();
+        saveConfig(config);
+      });
     });
-
-    selectJq.change(function () {
-      persistCustomization(selectJq);
-    });
-  });
 }
 
-function getCustomizations(callback) {
-  debugger;
-  chrome.runtime.getPackageDirectoryEntry(function (directoryEntry) {
-    debugger;
-    directoryEntry.getDirectory('customization', {}, function (subDirectoryEntry) {
-      debugger;
-      var directoryReader = subDirectoryEntry.createReader();
-      var folderNames = [];
-      (function readNext() {
-        debugger;
-        directoryReader.readEntries(function (entries) {
-          debugger;
-          if (entries.length) {
-            for (var i = 0; i < entries.length; ++i) {
-              if (entries[i].isDirectory==false) continue;
-              folderNames.push(entries[i].name);
+function getCustomizations() {
+  return new Promise((resolve) => {
+    chrome.runtime.getPackageDirectoryEntry(function (directoryEntry) {
+      directoryEntry.getDirectory('customization', {}, function (subDirectoryEntry) {
+        const folderNames = [];
+        const directoryReader = subDirectoryEntry.createReader();
+        (function readNext() {
+          directoryReader.readEntries(function (entries) {
+            if (entries.length) {
+              for (var i = 0; i < entries.length; i++) {
+                if (entries[i].isDirectory == false) continue;
+                folderNames.push(entries[i].name);
+              }
+              readNext();
+            } else {
+              resolve(folderNames);
             }
-            readNext();
-          } else {
-            callback(folderNames);
-          }
-        });
-      })();
+          });
+        })();
+      });
     });
-  });
-}
-
-function persistCustomization(selectJq) {
-  const data = {};
-  data[keyCustomization] = selectJq.val();
-  chrome.storage.local.set(data, function () {
-    console.log(`SET ${keyCustomization}=${data[keyCustomization]}`);
   });
 }
 
 $(function () {
-  const customizationSelectJq = $('select[name=customization]');
-  initCustomization(customizationSelectJq);
+  getConfig()
+    .then(c => config = c)
+    .then(() => {
+      const customizationSelectJq = $('select[name=customization]');
+      initCustomization(customizationSelectJq);
+    })
 });
