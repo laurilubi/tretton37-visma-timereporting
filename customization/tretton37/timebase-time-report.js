1177ea58a3527e5dc137a586ee7981406f8fb1d4
Lauri Lubi <lauri@lubi.se>
2020-08-21 17:53:22 +0200

    form clarity

--- customization/tretton37/timebase-time-report.js
+++ customization/tretton37/timebase-time-report.js
@@ -8,4 +8,23 @@ $(function () {
             $(this).text("Favourites");
         });
     });
+
+    $("#tblFormIcons td:has(input[name=finished])").each(function () {
+        var $td = $(this);
+        $td.css("padding-right", "50px");
+
+        const html = $td.html();
+        const label = `<label>${html}</label>`;
+        $td.html(label);
+
+        $("input", $td).css("margin-right", "3px");
+
+        var $save = $td.next().next().next();
+        $save.each(function () {
+            $save.css("padding-right", "50px");
+            $save.insertAfter($td);
+
+            $save.nextAll().css("opacity", 0.2);
+        });
+    });
 });
